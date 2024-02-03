bb7889ac88ef656818b05b1103f3bcac958e2ac8
ykmxxi <ykmxxi97@gmail.com>
2024-02-03 17:09:46 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 304 ms, Memory: 24516 KB -BaekjoonHub

--- 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.java
@@ -1,59 +1,71 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+/*
+    - 0 ~ N을 가로등 M개 설치 -> 위치 x 들
+    - 최소한의 높이로 굴다리 모든 길을 밝히려고 한다
+    - 단, 가로등은 모두 높이가 같고 정수
+    - 가로등 길이가 H -> 왼쪽으로 H, 오른쪽으로 H -> x - H ~ x + H
+ */
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
-	static StringTokenizer st;
 
-	static int N, M;
-	static int[] A;
+    static int N; // 굴다리 길이 1 ~ 100,000
+    static int M; // 1 <= M <= N
+    static int[] A; // 가로등 설치 위치
 
-	static void input() throws IOException {
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
-		A = new int[M + 1];
+    static void input() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            N = Integer.parseInt(br.readLine());
 
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 1; i <= M; i++) {
-			A[i] = Integer.parseInt(st.nextToken());
-		}
-	}
+            M = Integer.parseInt(br.readLine());
 
-	static boolean determination(int height) {
-		int last = 0;
-		for (int i = 1; i <= M; i++) {
-			if (A[i] - last <= height) {
-				last = A[i] + height;
-			} else {
-				return false;
-			}
-		}
-		return last >= N;
-	}
+            A = new int[M];
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int i = 0; i < M; i++) {
+                A[i] = Integer.parseInt(st.nextToken());
+            }
 
-	static void pro() {
-		Arrays.sort(A, 1, M + 1);
-		int L = 1, R = N, answer = N;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-		while (L <= R) {
-			int mid = (L + R) / 2;
-			if (determination(mid)) {
-				answer = mid;
-				R = mid - 1;
-			} else {
-				L = mid + 1;
-			}
-		}
+    static boolean can(int H) {
+        int before = 0;
 
-		System.out.println(answer);
-	}
+        for (int x : A) {
+            if (x - H > before) {
+                return false;
+            }
+            before = x + H;
+        }
 
-	public static void main(String[] args) throws IOException {
-		input();
-		pro();
-	}
+        return before >= N;
+    }
+
+    static void pro() {
+        int L = 1, R = N;
+        int ans = N;
+
+        while (L <= R) {
+            int mid = (L + R) / 2;
+
+            if (can(mid)) { // mid로 모두 비출 수 있으면
+                ans = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
 }
