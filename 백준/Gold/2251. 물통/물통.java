bba6d9d84c286486542520fac2be89f040f7cdae
ykmxxi <ykmxxi97@gmail.com>
2024-02-10 17:36:36 +0900

    [Gold V] Title: 물통, Time: 124 ms, Memory: 23296 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -3,8 +3,10 @@
     - 총 몇 마리의 지렁이가 필요한지 구하기
  */
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -15,6 +17,7 @@ public class Main {
     static boolean[][][] visit = new boolean[201][201][201];
 
     static class State {
+
         int[] A;
 
         State(int[] A) {
@@ -25,7 +28,7 @@ public class Main {
         }
 
         State move(int from, int to, int[] limit) {
-            int[] na = new int[] {A[0], A[1], A[2]};
+            int[] na = new int[]{A[0], A[1], A[2]};
 
             if (A[from] + A[to] > limit[to]) {
                 na[from] -= limit[to] - na[to];
@@ -36,6 +39,7 @@ public class Main {
             }
             return new State(na);
         }
+
     }
 
     static void input() throws IOException {
@@ -44,44 +48,40 @@ public class Main {
             limit[i] = Integer.parseInt(st.nextToken());
         }
     }
-    
-    static void bfs(int a, int b, int c) {
-        Queue<State> q = new LinkedList<>();
+
+    static void dfs(int a, int b, int c) {
         visit[a][b][c] = true;
-        q.add(new State(new int[]{a,b,c}));
-        
-        while (!q.isEmpty()) {
-            State state = q.poll();
-            
-            if (state.A[0] == 0) { // A 물통이 비어있으면 체크
-                possible[state.A[2]] = true;
-            }
-            
-            for (int from = 0; from < 3; from++) {
-                for (int to = 0; to < 3; to++) {
-                    if (from == to) {
-                        continue;
-                    }
-                    
-                    State next = state.move(from, to, limit);
-                    
-                    if (!visit[next.A[0]][next.A[1]][next.A[2]]) {
-                        visit[next.A[0]][next.A[1]][next.A[2]] = true;
-                        q.add(next);
-                    }
+        State state = new State(new int[]{a, b, c});
+
+        if (state.A[0] == 0) { // A 물통이 비어있으면 체크
+            possible[state.A[2]] = true;
+        }
+
+        for (int from = 0; from < 3; from++) {
+            for (int to = 0; to < 3; to++) {
+                if (from == to) {
+                    continue;
+                }
+
+                State next = state.move(from, to, limit);
+
+                if (!visit[next.A[0]][next.A[1]][next.A[2]]) {
+                    dfs(next.A[0], next.A[1], next.A[2]);
                 }
             }
         }
+
+
     }
 
     static void pro() {
-        bfs(0, 0, limit[2]);
+        dfs(0, 0, limit[2]);
         for (int i = 0; i <= limit[2]; i++) {
             if (possible[i]) {
                 sb.append(i).append(' ');
             }
         }
-        
+
         System.out.println(sb);
     }
 
@@ -89,4 +89,5 @@ public class Main {
         input();
         pro();
     }
+
 }
