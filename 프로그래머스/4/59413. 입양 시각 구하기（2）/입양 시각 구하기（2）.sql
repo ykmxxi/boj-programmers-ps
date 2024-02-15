a924d848f57ae8d12da18d9506eb2ecac16c1bc7
ykmxxi <ykmxxi97@gmail.com>
2024-02-15 16:59:41 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,12 @@
+-- 몇 시에 입양이 가장 활발하게 일어나는지 알아보려 한다.
+-- 0시 부터 23시 까지, 각 시간대별로 입양이 몇 건 발생했는지 조회
+-- 결과는 시간대 순으로 정렬
+
+SET @hour := -1;
+SELECT (@hour := @hour + 1) AS HOUR
+	,(SELECT COUNT(ANIMAL_ID)
+      FROM ANIMAL_OUTS
+      WHERE HOUR(DATETIME) = @hour) AS COUNT 
+FROM ANIMAL_OUTS
+WHERE @hour < 23
+order by HOUR;
