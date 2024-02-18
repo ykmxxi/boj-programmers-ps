23cfc41704524ff56dd3d5cf40b17532962f6799
ykmxxi <ykmxxi97@gmail.com>
2024-02-18 16:01:06 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,7 @@
+-- 입양을 간 기록이 있는데, 보호소에 들어온 기록이 없는 동물의 id, 이름
+-- id 오름차순
+
+select o.ANIMAL_ID, o.NAME
+from ANIMAL_OUTS as o left join ANIMAL_INS as i on o.ANIMAL_ID = i.ANIMAL_ID
+where i.ANIMAL_ID is null
+order by ANIMAL_ID;
