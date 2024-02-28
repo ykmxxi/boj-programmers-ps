b4846b3085fe48472fbed4f9cd47dabb543dfe5a
ykmxxi <ykmxxi97@gmail.com>
2024-02-28 17:38:28 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.47 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,52 @@
+/*
+    - "l 숫자": 큐에 숫자 삽입
+    - "D 1": 최대값 삭제, "D -1": 최소값 삭제
+    
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); // 오름차순
+        PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder()); // 내림차순
+        
+        for (String opr : operations) {
+            StringTokenizer st = new StringTokenizer(opr, " ");
+            
+            String s1 = st.nextToken();
+            int n = Integer.parseInt(st.nextToken());
+            
+            if ("I".equals(s1)) { // 삽입
+                pq.offer(n);
+                maxPq.offer(n);
+            } else { // 삭제 연산
+                if (pq.isEmpty()) { // 비어있으면 무시
+                    continue;
+                }
+                if (n == 1) { // 최대값 삭제
+                    int max = maxPq.poll();
+                    pq.remove(max);
+                } else { // 최소값 삭제
+                    int min = pq.poll();
+                    maxPq.remove(min);
+                }
+            }
+        }
+        
+        int[] ans = new int[2];
+        if (pq.size() >= 2) {
+            ans[0] = maxPq.poll();
+            ans[1] = pq.poll();
+        } else if (pq.size() == 1) {
+            int n = pq.poll();
+            ans[0] = n;
+            ans[1] = n;
+        } else {
+            ans[0] = 0;
+            ans[1] = 0;
+        }
+        return ans;
+        
+    }
+}
