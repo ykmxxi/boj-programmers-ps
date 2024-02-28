ad5a9a4f3384bd088458e2a49f8ebcbb5564c4ca
ykmxxi <ykmxxi97@gmail.com>
2024-02-28 16:32:57 +0900

    [level 3] Title: 베스트앨범, Time: 3.00 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,52 +1,60 @@
+/*
+    - 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범 출시
+    - 노래: 고유 번호로 구분
+    - 수록 기준
+        1. 속한 노래가 가장 <많이 재생된 장르> 수록
+        2. 장르 내에서 <많이 재생된> 노래를 먼저 수록
+        3. 장르 내에서 횟수가 같으면 고유 번호가 낮은 노래를 수록
+*/
+
 import java.util.*;
 
 class Solution {
-    public int[] solution(String[] genres, int[] plays) {
-        List<Integer> ans = new ArrayList<>();
-        Map<String, Integer> totalCnt = new HashMap<>();
-        Map<String, List<Info>> infos = new HashMap<>();
-        
-        for (int i = 0; i < genres.length; i++) {
-            totalCnt.put(genres[i], totalCnt.getOrDefault(genres[i], 0) + plays[i]);
-            infos.put(genres[i], new ArrayList<>());
-        }
-        
-        for (int i = 0; i < genres.length; i++) {
-            infos.get(genres[i]).add(new Info(plays[i], i));
-        }
-        
-        List<String> keys = new ArrayList<>(totalCnt.keySet());
-        keys.sort((o1, o2) -> totalCnt.get(o2) - totalCnt.get(o1));
-        
-        for (String key : keys) {
-            List<Info> cnts = infos.get(key);
-            Collections.sort(cnts);
-            
-            if (cnts.size() > 1) {
-                ans.add(cnts.get(0).num);
-                ans.add(cnts.get(1).num);
-            } else {
-                ans.add(cnts.get(0).num);
-            }
-            
-        }
-        
-        return ans.stream().mapToInt(i -> i).toArray();
-    }
     
     static class Info implements Comparable<Info> {
-        int plays;
         int num;
+        int play;
         
-        Info(int plays, int num) {
-            this.plays = plays;
+        Info(int num, int play) {
             this.num = num;
+            this.play = play;
         }
         
         @Override
         public int compareTo(Info o) {
-            return o.plays - this.plays;
+            if (this.play != o.play) {
+                return o.play - this.play; // 재생 횟수 내림차순
+            }
+            return this.num - o.num; // 고유 번호 오름차순
         }
+        
     }
     
+    public int[] solution(String[] genres, int[] plays) {
+        Map<String, Integer> counts = new HashMap<>();
+        Map<String, List<Info>> infos = new HashMap<>();
+        
+        for (int i = 0; i < genres.length; i++) {
+            String genre = genres[i];
+            counts.put(genre, counts.getOrDefault(genre, 0) + plays[i]);
+            infos.computeIfAbsent(genre, k -> new ArrayList<>()).add(new Info(i, plays[i]));
+        }
+        
+        List<String> keys = new ArrayList<>(counts.keySet());
+        keys.sort((o1, o2) -> counts.get(o2) - counts.get(o1)); // 장르별 총 재생횟수 내림차순 정렬
+        List<Integer> ans = new ArrayList<>();
+        
+        for (String k : keys) {
+            List<Info> l = infos.get(k);
+            Collections.sort(l);
+            
+            ans.add(l.get(0).num);
+            if (l.size() >= 2) {
+                ans.add(l.get(1).num);
+            }
+        }
+        
+        return ans.stream().mapToInt(i -> i).toArray();
+        
+    }
 }
