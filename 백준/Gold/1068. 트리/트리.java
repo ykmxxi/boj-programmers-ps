052274a464826b6d35c823c1583a19cf588a96c6
ykmxxi <ykmxxi97@gmail.com>
2024-04-13 10:35:07 +0900

    [Gold V] Title: 트리, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.java
+++ 백준/Gold/1068. 트리/트리.java
@@ -1,69 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+/*
+    - 노드 하나를 지울 때 남은 트리에서 리프 노드의 개수를 구하는 프로그램
+    - 리프 노드 -> 인접한 점이 오직 부모만 있을 때
+ */
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
 
-	static int N, root, erased;
-	static ArrayList<Integer>[] child;
-	static int[] leaf;
+    static int N, root, remove;
+    static List<Integer>[] child;
+    static int[] leaf;
 
-	static void input() throws IOException {
-		N = Integer.parseInt(br.readLine());
-		child = new ArrayList[N];
-		leaf = new int[N];
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < N; i++) {
-			child[i] = new ArrayList<>();
-		}
+        child = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            child[i] = new ArrayList<>();
+        }
 
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			int node = Integer.parseInt(st.nextToken());
-			if (node == -1) {
-				root = i;
-				continue;
-			}
-			child[node].add(i);
-		}
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        leaf = new int[N];
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            if (n == -1) {
+                root = i;
+                continue;
+            }
+            child[n].add(i); // n의 자식은 i
+        }
 
-		erased = Integer.parseInt(br.readLine());
-	}
+        remove = Integer.parseInt(br.readLine());
+    }
 
-	static void dfs(int x, int par) {
-		if (child[x].isEmpty()) {
-			leaf[x] = 1;
-		}
+    static void dfs(int x, int par) {
+        if (child[x].isEmpty()) {
+            leaf[x] = 1;
+        }
 
-		for (int y : child[x]) {
-			if (y == par) {
-				continue;
-			}
-			dfs(y, x);
-			leaf[x] += leaf[y];
-		}
-	}
+        for (int y : child[x]) {
+            if (y == par) {
+                continue;
+            }
 
-	static void pro() {
-		for (int i = 0; i < N; i++) {
-			if (child[i].contains(erased)) {
-				child[i].remove(child[i].indexOf(erased));
-			}
-		}
+            dfs(y, x);
+            leaf[x] += leaf[y];
+        }
+    }
 
-		if (root != erased) {
-			dfs(root, -1);
-		}
+    static void pro() {
+        for (int i = 0; i < N; i++) {
+            if (child[i].contains(remove)) {
+                child[i].remove(child[i].indexOf(remove));
+            }
+        }
 
-		System.out.println(leaf[root]);
-	}
+        if (root != remove) {
+            dfs(root, -1);
+        }
 
-	public static void main(String[] args) throws IOException {
-		input();
-		pro();
-	}
+        System.out.println(leaf[root]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
+
 }
