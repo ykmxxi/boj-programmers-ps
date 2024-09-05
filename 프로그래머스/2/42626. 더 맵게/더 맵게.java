f9fa7d7cdfc78a0865f880fe96e5fdbe7c660062
ykmxxi <ykmxxi97@gmail.com>
2024-09-05 10:50:17 +0900

    [level 2] Title: 더 맵게, Time: 1749.58 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,28 +1,37 @@
-import java.util.PriorityQueue;
+import java.util.*;
+
+/*
+    - 스코빌 지수 K 이상으로 만들기, 스코빌 지수가 가장 낮은 두 개의 음식을 섞음
+    - 섞은 음식 = 가장 안매운 음식 + (두 번째로 안매운 음식 * 2)
+    - 배열의 길이: 2 ~ 10^6, K(기준값): 0 ~ 10^9
+    - 배열의 각 요소: 0 ~ 10^6
+    - 최악의 경우: 모두 10^6, 10^6 길이 -> 2*10^6을 10^6번 더하기 -> long
+*/
 
 class Solution {
-	public int solution(int[] scoville, int K) {
-		int answer = 0;
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-		for (int num : scoville) {
-			pq.add(num);
-		}
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Long> pq = new PriorityQueue<>(); // 오름차순 정렬
+        for (int sco : scoville) {
+            pq.offer((long) sco);
+        }
+        
+        long min = pq.peek();
+        while (min < K) {
+            if (pq.isEmpty() || pq.size() == 1) {
+                return -1;
+            }
+            long nf = mix(pq.poll(), pq.poll());
+            pq.offer(nf);
+            min = pq.peek();
+            answer++;
+        }
+        return answer;
+    }
+    
+    public long mix(long a, long b) {
+        return a + (2 * b);
+    }
 
-		while (true) {
-			if (pq.size() == 1 && pq.peek() < K) {
-				answer = -1;
-				break;
-			}
-			if (pq.peek() >= K) { // 스코빌 지수가 가장 낮은 음식이 K 이상이면 stop
-				break;
-			}
-
-			int sum = pq.poll() + pq.poll() * 2;
-			pq.add(sum);
-			answer++;
-		}
-
-		return answer;
-	}
 }
