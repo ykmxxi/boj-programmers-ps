17c77612537e9a942dae3b8d181d24b3730c7d7a
ykmxxi <ykmxxi97@gmail.com>
2024-09-09 10:19:22 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.25 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,23 +1,33 @@
+/*
+    - 한 명의 선수를 제외하고 완주, 완주하지 못한 선수의 이름을 찾기
+    - participant 길이 1 ~ 100,000, completion 길이는 participant 길이 - 1
+    -
+*/
+
 import java.util.*;
 
 class Solution {
+
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String, Integer> hm = new HashMap<>();
-        
-        for (String player : participant) {
-          hm.put(player, hm.getOrDefault(player, 0) + 1);  
-        }
-        
-        for (String player : completion) {
-            hm.put(player, hm.get(player) - 1);
+        Map<String, Integer> map = new HashMap<>();
+        for (String key : participant) {
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
 
-        for (Map.Entry<String, Integer> entry : hm.entrySet()) {
-            if (entry.getValue() == 1) {
-                answer = entry.getKey();
+        for (String com : completion) {
+            if (map.containsKey(com)) {
+                int cnt = map.get(com) - 1;
+                map.put(com, cnt);
+            }
+        }
+
+        for (String key : map.keySet()) {
+            if (map.get(key) > 0) {
+                return key;
             }
         }
         return answer;
     }
+
 }
