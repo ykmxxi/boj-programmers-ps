de2ed49d76ccbf0c6a3e45979173fa168a3eb0f8
ykmxxi <ykmxxi97@gmail.com>
2024-09-12 14:57:44 +0900

    [level 3] Title: 여행경로, Time: 1.13 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -3,39 +3,32 @@
     - 모든 공항은 알파벳 대문자 3글자, 공항 수는 3 ~ 10,000
     - tickets의 각 행은 a -> b (방향 그래프)
     - 주어진 항공권을 모두 사용해야 한다
-    
 */
 
 import java.util.*;
 
 class Solution {
     
-    List<String> ans;
-    boolean[] visit;
-    
-    void dfs(String cur, String route, String[][] tickets, int depth) {
-        if (depth == tickets.length) {
-            ans.add(route);
-        } else {
-            for (int i = 0; i < tickets.length; i++) {
-                if (cur.equals(tickets[i][0]) && !visit[i]) { // 현재 공항이 티켓의 출발점과 같으면
-                    visit[i] = true; // 해당 경로 방문 처리
-                    dfs(tickets[i][1], route + " " + tickets[i][1], tickets, depth + 1); // 다음 공항으로 이동
-                    visit[i] = false; // 해당 경로가 끝나면 방문 처리 초기화
-                }
-            }
-        }
-    }
+    Map<String, Queue<String>> adj;
     
     public String[] solution(String[][] tickets) {
-        ans = new ArrayList<>();
-        visit = new boolean[tickets.length];
+        // 인접리스트 구성
+        adj = new HashMap<>();
+        for (String[] t : tickets) {
+            adj.computeIfAbsent(t[0], val -> new PriorityQueue<>()).offer(t[1]);
+        }
         
-        dfs("ICN", "ICN", tickets, 0); // 처음 시작점은 무조건 ICN, 처음 경로도 ICN
+        List<String> ans = new LinkedList<>();
+        dfs("ICN", ans);
         
-        Collections.sort(ans);
-        
-        return ans.get(0).split(" ");
+        return ans.toArray(new String[0]);
     }
     
+    public void dfs(String airport, List<String> ans) {
+        while (adj.containsKey(airport) && !adj.get(airport).isEmpty()) {
+            dfs(adj.get(airport).poll(), ans);
+        }
+        ans.add(0, airport);
+    }
+
 }
