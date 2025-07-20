a48443f9d71d08e26a70e7aef55f3dbbdf739fec
posty <ykmxxi97@gmail.com>
2025-07-20 21:29:51 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 22240 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,50 +1,58 @@
-import java.util.*;
+// https://www.acmicpc.net/problem/15649
+// 순열
+// 시간 복잡도: O(N! / (N - M)!) -> 최악의 경우 8개를 고르면? (8! / 0!)
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int n, m;
-    static int[] arr, used;
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
-
-    static void input() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        arr = new int[m];
-        used = new int[n + 1];
-    }
-
-    static void rec_func(int depth) {
-        if (depth == m) {
-            for (int a : arr) {
-                sb.append(a).append(' ');
-            }
-            sb.append('\n');
-        } else {
-            for (int cand = 1; cand <= n; cand++) {
-                if (used[cand] == 1) {
-                    continue;
-                }
-                arr[depth] = cand;
-                used[cand] = 1; // 사용 처리
-
-                rec_func(depth + 1);
-
-                arr[depth] = 0;
-                used[cand] = 0; // 사용이 끝나면 초기화
-            }
-        }
-    }
+    static int N, M;
+    static int[] sel;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         input();
+        pro();
+        System.out.print(sb);
+    }
 
-        rec_func(0);
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        System.out.print(sb.toString());
+        sel = new int[M];
+        visited = new boolean[N + 1];
+    }
+
+    static void pro() {
+        rec(0);
+    }
+
+    static void rec(int depth) {
+        if (depth == M) {
+            for (int i = 0 ; i < M; i++) {
+                sb.append(sel[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int cand = 1; cand<=N; cand++) {
+                if (visited[cand]) { // 이미 고른 숫자라면 중복 X
+                    continue;
+                }
+                sel[depth] = cand;
+                visited[cand] = true;
+
+                rec(depth + 1);
+
+                // 초기화
+                sel[depth] = 0;
+                visited[cand] = false;
+            }
+        }
     }
 }
