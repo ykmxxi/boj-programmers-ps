286c9247c3a1d1bc3259d3dbfe12f7e71553a0d2
posty <ykmxxi97@gmail.com>
2025-08-16 10:52:54 +0900

    [Silver I] Title: 단지번호붙이기, Time: 100 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,80 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
+// 단지번호붙이기: https://www.acmicpc.net/problem/2667
+// 시간 복잡도: O(N^2 logN) -> N의 최대값은 5^2 -> 5^4 -> O(1)에 가까움 상수 시간에
+
+import java.io.*;
+import java.util.*;
 
 public class Main {
+
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
-    static StringTokenizer st;
-
-    static int N, group_cnt;
-    static String[] A;
+    static int N, size;
+    static int[][] arr, dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     static boolean[][] visit;
-    static ArrayList<Integer> group;
-    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우
+    static List<Integer> ans = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
 
     static void input() throws IOException {
         N = Integer.parseInt(br.readLine());
-        A = new String[N];
 
-        for (int i = 0; i < N; i++) {
-            A[i] = br.readLine();
-        }
+        arr = new int[N][N];
         visit = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            char[] ch = br.readLine().toCharArray();
+            for (int j = 0; j < ch.length; j++) {
+                arr[i][j] = ch[j] - '0';
+            }
+        }
+    }
+
+    static void pro() {
+        // 1은 집, 0은 빈 곳 -> 단지의 수를 오름차순으로 정렬해 출력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == 0 || visit[i][j]) {
+                    continue;
+                }
+                size = 0;
+                dfs(i, j);
+                ans.add(size);
+            }
+        }
+
+        Collections.sort(ans);
+        sb.append(ans.size()).append('\n');
+        for (int a : ans) {
+            sb.append(a).append('\n');
+        }
+        System.out.print(sb);
     }
 
     static void dfs(int x, int y) {
-        group_cnt++;
         visit[x][y] = true;
+        size++;
 
-        // 인접한 집으로 새로운 방문
         for (int k = 0; k < 4; k++) {
-            // (x, y) -> dir[k]
             int nx = x + dir[k][0];
             int ny = y + dir[k][1];
 
             if (nx < 0 || ny < 0 || nx >= N || ny >= N) {
                 continue;
             }
-            if (A[nx].charAt(ny) == '0') {
-                continue;
-            }
-            if (visit[nx][ny]) {
+            if (visit[nx][ny] || arr[nx][ny] == 0) {
                 continue;
             }
             dfs(nx, ny);
         }
-
-    }
-
-    static void pro() {
-        group = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (!visit[i][j] && A[i].charAt(j) == '1') {
-                    group_cnt = 0;
-                    dfs(i, j);
-                    group.add(group_cnt);
-                }
-            }
-        }
-
-        Collections.sort(group);
-        sb.append(group.size()).append('\n');
-        for (int cnt : group) {
-            sb.append(cnt).append('\n');
-        }
-
-        System.out.println(sb);
-
-    }
-
-    public static void main(String[] args) throws IOException {
-        input();
-        pro();
     }
 }
