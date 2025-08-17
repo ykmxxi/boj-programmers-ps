3952b36cc5a8cb0adb65aec19963a0b5e4eda994
posty <ykmxxi97@gmail.com>
2025-08-17 11:30:57 +0900

    [Gold IV] Title: 저울, Time: 132 ms, Memory: 14892 KB -BaekjoonHub

--- 백준/Gold/10159. 저울/저울.java
+++ 백준/Gold/10159. 저울/저울.java
@@ -1,5 +1,5 @@
 // 저울: https://www.acmicpc.net/problem/10159
-// 시간 복잡도: O(N *(N + M)) -> 정점마다 bfs 2번
+// 시간 복잡도: O(N^3) -> 불리언 전이 폐쇄(Floyd–Warshall)
 
 import java.io.*;
 import java.util.*;
@@ -10,8 +10,7 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
     static int N, M;
-    static List<Integer>[] adj1, adj2;
-    static boolean[] visit;
+    static boolean[][] reach; // reach[u][v] = (u > v 를 알 수 있음)
 
     public static void main(String[] args) throws IOException {
         input();
@@ -19,54 +18,48 @@ public class Main {
     }
 
     static void input() throws IOException {
-        N = Integer.parseInt(br.readLine()); // 정점의 개수
-        M = Integer.parseInt(br.readLine()); // 간선의 개수
+        N = Integer.parseInt(br.readLine()); // 물건(정점) 개수
+        M = Integer.parseInt(br.readLine()); // 비교(간선) 개수
 
-        adj1 = new ArrayList[N + 1];
-        adj2 = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            adj1[i] = new ArrayList<>(); // 가벼운것과 이어지는 단방향 그래프
-            adj2[i] = new ArrayList<>(); // 무것운것과 이어지는 단방향 그래프
-        }
+        reach = new boolean[N + 1][N + 1];
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            adj1[x].add(y);
-            adj2[y].add(x);
+            int a = Integer.parseInt(st.nextToken()); // a > b
+            int b = Integer.parseInt(st.nextToken());
+            reach[a][b] = true; // a가 더 무거움(a -> b)
         }
     }
 
     static void pro() {
+        floyd(); // 전이 폐쇄
+
         for (int i = 1; i <= N; i++) {
-            visit = new boolean[N + 1];
-            int a = bfs(adj1, i);
-            int b = bfs(adj2, i);
-            sb.append(N - 1 - a - b).append('\n');
+            int heavier = 0; // j > i 를 알 수 있는 j 개수 = 열 i에서 true 개수
+            int lighter = 0; // i > j 를 알 수 있는 j 개수 = 행 i에서 true 개수
+
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                if (reach[i][j]) lighter++;
+                if (reach[j][i]) heavier++;
+            }
+
+            int unknown = (N - 1) - lighter - heavier;
+            sb.append(unknown).append('\n');
         }
 
         System.out.print(sb);
     }
 
-    static int bfs(List<Integer>[] adj, int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        visit[start] = true;
-        int dist = 0;
-
-        while (!q.isEmpty()) {
-            int x = q.poll();
-            for (int y : adj[x]) {
-                if (visit[y]) {
-                    continue;
+    // Floyd–Warshall(불리언): i->k, k->j 가 되면 i->j 도 된다
+    static void floyd() {
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                if (!reach[i][k]) continue; // 미세 최적화
+                for (int j = 1; j <= N; j++) {
+                    if (reach[k][j]) reach[i][j] = true;
                 }
-                visit[y] = true;
-                dist++;
-                q.offer(y);
             }
         }
-
-        return dist;
     }
 }
