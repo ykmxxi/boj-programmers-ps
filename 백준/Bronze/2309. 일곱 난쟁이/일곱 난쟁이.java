59f0f8af15fd272751bf5c6cecc02c328fd1ef2f
posty <ykmxxi97@gmail.com>
2026-01-13 17:12:08 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 100 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,58 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringBuilder sb = new StringBuilder();
 
-	static int[] A;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static boolean flag = false;
+    static int[] A;
+    static int[] B;
 
-	static void input() throws IOException {
-		A = new int[10];
-		for (int i = 1; i <= 9; i++) {
-			A[i] = Integer.parseInt(br.readLine());
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
 
-	static boolean isHundred(int idx1, int idx2) {
-		int sum = 0;
-		for (int i = 1; i <= 9; i++) {
-			if (i == idx1 || i == idx2)
-				continue;
-			sum += A[i];
-		}
+    static void input() throws IOException {
+        A = new int[9];
+        B = new int[7];
+        for (int i = 0; i < 9; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+    }
 
-		return sum == 100;
-	}
+    static void pro() {
+        // BF: 조합 문제 9명 중 7개 중복없이 선택
+        Arrays.sort(A);
+        rec(0, -1, 0);
+    }
 
-	static void pro() {
-		Arrays.sort(A);
-		int idx1 = 0;
-		int idx2 = 0;
-
-		for (int i = 1; i < 9; i++) {
-			for (int j = i + 1; j <= 9; j++) {
-				if (isHundred(i, j)) {
-					idx1 = i;
-					idx2 = j;
-					break;
-				}
-			}
-		}
-
-		for (int i = 1; i <= 9; i++) {
-			if (i == idx1 || i == idx2)
-				continue;
-			sb.append(A[i]).append('\n');
-		}
-
-		System.out.print(sb);
-	}
-
-	public static void main(String[] args) throws IOException {
-		input();
-		pro();
-	}
+    static void rec(int len, int prev, int sum) {
+        if (flag) {
+            return;
+        }
+        if (len == 7) {
+            if (sum == 100) {
+                flag = true;
+                for (int num : B) {
+                    sb.append(num).append('\n');
+                }
+                System.out.print(sb);
+            }
+            return;
+        }
+        for (int i = prev + 1; i < 9; i++) {
+            B[len] = A[i];
+            rec(len + 1, i, sum + A[i]);
+            B[len] = 0;
+        }
+    }
 }
