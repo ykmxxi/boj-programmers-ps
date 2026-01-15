42c611d3cf45ce42002e03039de7a8eb77990b26
posty <ykmxxi97@gmail.com>
2026-01-15 14:35:53 +0900

    [Silver II] Title: 에디터, Time: 396 ms, Memory: 58296 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,70 +1,54 @@
 import java.util.*;
 import java.io.*;
 
-// 시간 복잡도: O(N*M) // N = 명령어의 개수, M = 문자열의 길이
-// 공간 복잡도: O(100,000) + O(600,000)
 public class Main {
-
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
-    static String str;
-    static String[] A;
-    static int N;
-    static Deque<Character> left, right;
-
     public static void main(String[] args) throws IOException {
-        input();
-        pro();
-    }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int M = Integer.parseInt(br.readLine());
 
-    static void input() throws IOException {
-        str = br.readLine();
-        N = Integer.parseInt(br.readLine());
-        A = new String[N];
-        for (int i = 0; i < N; i++) {
-            A[i] = br.readLine();
-        }
-    }
-
-    static void pro() {
-        // 최대 600,000 길이 글자
-        // 맨 앞, 맨 뒤, 문장 중간 -> 길이가 L 문자열에 커서가 위치할 수 있는 곳은 L + 1
-        // L(왼쪽 1칸), D(오른쪽 한 칸), B(커서 왼쪽 문자 삭제), P $ ($ 문자를 커서 왼쪽에 추가)
-        left = new ArrayDeque<>();
-        right = new ArrayDeque<>();
-
+        // 1. LinkedList 준비 (Doubly Linked List 기반)
+        LinkedList<Character> list = new LinkedList<>();
         for (char ch : str.toCharArray()) {
-            left.push(ch);
+            list.add(ch);
         }
 
-        for (String com : A) { // O(100,000)
-            command(com);
+        // 2. ListIterator 생성 (커서를 리스트의 맨 끝으로 설정)
+        // list.size()를 인자로 주면 초기 커서가 마지막 요소 다음에 위치함
+        ListIterator<Character> iter = list.listIterator(list.size());
+
+        for (int i = 0; i < M; i++) {
+            String command = br.readLine();
+            char op = command.charAt(0);
+
+            switch (op) {
+                case 'L':
+                    if (iter.hasPrevious()) {
+                        iter.previous(); // 커서를 왼쪽으로 이동
+                    }
+                    break;
+                case 'D':
+                    if (iter.hasNext()) {
+                        iter.next(); // 커서를 오른쪽으로 이동
+                    }
+                    break;
+                case 'B':
+                    if (iter.hasPrevious()) {
+                        iter.previous(); // 삭제 대상을 가리키기 위해 왼쪽으로 이동 후
+                        iter.remove();   // 해당 요소 삭제
+                    }
+                    break;
+                case 'P':
+                    iter.add(command.charAt(2)); // 현재 커서 위치에 추가 (커서는 추가된 문자 뒤로 자동 이동)
+                    break;
+            }
         }
 
-        while (!left.isEmpty()) {
-            right.push(left.pop());
-        }
-        while (!right.isEmpty()) {
-            sb.append(right.pop());
+        // 3. 결과 출력 (StringBuilder 활용)
+        StringBuilder sb = new StringBuilder();
+        for (char ch : list) {
+            sb.append(ch);
         }
         System.out.print(sb);
-    }
-
-    static void command(String com) {
-        if (com.equals("L")) {
-            if (!left.isEmpty()) {
-                right.push(left.pop());
-            }
-        } else if (com.equals("D")) {
-            if (!right.isEmpty()) {
-                left.push(right.pop());
-            }
-        } else if (com.equals("B")) {
-            if (!left.isEmpty()) {
-                left.pop();
-            }
-        } else {
-            left.push(com.charAt(2));
-        }
     }
 }
