7317716701a8f6cb6bf58e0c878d6f7949236734
posty <ykmxxi97@gmail.com>
2026-02-02 14:27:54 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16692 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,52 +1,46 @@
-// https://www.acmicpc.net/problem/10799
-// 시간 복잡도: O(n), n <= 100,000
-// 쇠막대기를 겹쳐 자른다, 쇠막대기는 자신보다 긴 것 위에만 놓인다 / 적어도 하나 잘라야 한다 / 레이저는 쇠막대기 양 끝점과 겹치지 않음
-// 레이저: 인접한 ()
-// 쇠막대기: 왼쪽 끝 (, 오른쪽 끝 ) -> 단, 레이저는 레이저는 쇠막대기 양 끝점과 겹치지 않음
-
 import java.util.*;
 import java.io.*;
 
+// 시간 복잡도: O(N), 길이(N: 1 ~ 100,000)
+// 공간 복잡도: O(N)
 public class Main {
 
-    static String S;
-    static Deque<Character> dq, dq2;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static String str;
 
     public static void main(String[] args) throws IOException {
         input();
-
         pro();
     }
 
     static void input() throws IOException {
-         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-         S = br.readLine();
-         dq = new ArrayDeque<>();
-         dq2 = new ArrayDeque<>();
-         for (char ch : S.toCharArray()) {
-             dq.push(ch);
-         }
+        str = br.readLine();
     }
 
     static void pro() {
-        // 레이저를 만나면 -> 닫히지 않는 괄호의 개수만큼 짤려 추가
-        // 레이저가 아닌 쇠막대기 왼쪽 끝을 만나면 1개 추가
+        // 자신보다 긴 쇠막대기 위에만 놓을 수 있음
         int ans = 0;
-        char before = ' ';
-        while (!dq.isEmpty()) {
-            char cur = dq.poll();
-            if (cur == '(' && before == ')') { // 레이저
-                dq2.poll();
-                ans += dq2.size(); // 레이저로 자름
-            } else if (cur == '(' && before != ')') { // 쇠막대기
-                dq2.poll();
-                ans++; // 쇠막대기 왼쪽 끝에 도달해 1개 추가
-            } else {
-                dq2.push(cur);
+        char prev = '(';
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char ch : str.toCharArray()) {
+            if (ch == '(') {
+                stack.push(ch);
+                prev = '(';
+            } else if (ch == ')') {
+                if (!stack.isEmpty() && prev == '(') { // 레이저
+                    stack.pop(); // 레이저 여는 괄호 제거 후
+                    ans += stack.size();
+                } else { // 레이저가 아님
+                    if (!stack.isEmpty()) {
+                        stack.pop();
+                        ans++;
+                    }
+                }
+                prev = ')';
             }
-            before = cur;
         }
 
-        System.out.println(ans);
+        System.out.print(ans);
     }
 }
