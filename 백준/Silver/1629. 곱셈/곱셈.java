bcdc57b1668206b0a5c2f39fc6cf225daed8d6ba
posty <ykmxxi97@gmail.com>
2026-02-09 15:33:36 +0900

    [Silver I] Title: 곱셈, Time: 92 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,36 +1,40 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.*;
+import java.util.*;
+
+// 시간 복잡도: O(log B)
+// 공간 복잡도: O(log B)
 public class Main {
- 
-	public static long C;	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		long A = Long.parseLong(st.nextToken());
-		long B = Long.parseLong(st.nextToken());
-		C = Long.parseLong(st.nextToken());
-		
-		System.out.println(pow(A, B));
-	}
-	
-	public static long pow(long A, long exponent) {
-		
-		if(exponent == 1) {
-			return A % C;
-		}
-		
-		long temp = pow(A, exponent / 2);
-		
-		if(exponent % 2 == 1) {
-			return (temp * temp % C) * A % C;
-		}
-		return temp * temp % C;
-		
-	}
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int A, B, C;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
+
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine(), " ");
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+    }
+
+    static void pro() {
+        System.out.print(pow(A, B, C));
+    }
+
+    static long pow(int a, int b, int c) {
+        if (b == 1) {
+            return a % c;
+        }
+        long value = pow(a, b/2, c);
+        value = (value * value) % c;
+        if (b % 2 == 0) {
+            return value;
+        }
+        return (value * a) % c;
+    }
 }
