0ed3ae1514764910189e2214d075189eeb844cb9
posty <ykmxxi97@gmail.com>
2026-02-10 14:50:01 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -1,48 +1,46 @@
-// https://www.acmicpc.net/problem/1074
-// 시간 복잡도: O(N)
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
+// 시간 복잡도: O(N)
+// 공간 복잡도: O(N)
 public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static int N, r, c, value;
+    static int N, r, c;
 
     public static void main(String[] args) throws IOException {
         input();
-
-        // 1,2,3,4 -> 1,2,3,4 형식으로 속한 사분면 찾기
-        rec((int) Math.pow(2, N), 0, 0);
+        pro();
     }
 
     static void input() throws IOException {
         st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
     }
 
-    static void rec(int len, int x, int y) {
-        if (len == 1) {
-            System.out.println(value);
-        } else {
-            int nl = len / 2;
-            if (r < x + nl && c < y + nl) { // 1사분면
-                rec(nl, x, y);
-            } else if (r < x + nl && c >= y + nl) { // 2사분면
-                value += nl * nl;
-                rec(nl, x, y + nl);
-            } else if (r >= x + nl && c < y + nl) { // 3사분면
-                value += (nl * nl) * 2;
-                rec(nl, x + nl, y);
-            } else { // 4사분면
-                value += (nl * nl) * 3;
-                rec(nl, x + nl, y + nl);
-            }
+    static void pro() {
+        System.out.print(rec(N , r, c));
+    }
+
+    static int rec(int n, int r, int c) {
+        if (n == 0) {
+            return 0;
         }
+        int half = 1 << (n - 1);
+        if (r < half && c < half) { // 1사분면
+            return rec(n - 1, r, c);
+        }
+        if (r < half && c >= half) { // 2사분면
+            return half * half + rec(n - 1, r, c - half);
+        }
+        if (r >= half && c < half) { // 3사분면
+            return 2 * half * half + rec(n - 1, r - half, c);
+        }
+        // 4사분면
+        return 3 * half * half + rec(n - 1, r - half, c- half);
     }
 }
-
