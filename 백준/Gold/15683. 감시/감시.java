6d674641d02f4f3cf34794d4ddeea69f8f73a671
posty <ykmxxi97@gmail.com>
2026-02-23 15:09:35 +0900

    [Gold III] Title: 감시, Time: 164 ms, Memory: 19612 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,173 +1,111 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
+// 시간 복잡도:
+// 공간 복잡도:
 public class Main {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static StringTokenizer st;
 
-	static int N, M, ans;
-	static int[] selected;
-	static int[][] A, B;
-	static ArrayList<Cctv> cctvList;
-	static int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // 상우하좌
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    static int N, M, ans = Integer.MAX_VALUE;
+    static int[][] adj, visit;
+    static List<int[]> cctv;
 
-	static class Cctv {
-		int type;
-		int x;
-		int y;
+    // 1번 1개씩, 2번: 13 or 24, 3번: 14 or 21 or 32 or 43
+    // 4번: 134 or 241 or 312 or 423, 5번: 1234
+    static int[][][] dirs = {
+        {},
+        {{0}, {1}, {2}, {3}},
+        {{0, 2}, {1, 3}},
+        {{0,3}, {1, 0}, {2, 1}, {3, 2}},
+        {{0,2,3}, {1,3,0}, {2,0,1}, {3,1,2}},
+        {{0,1,2,3}}
+    };
 
-		public Cctv(int type, int x, int y) {
-			this.type = type;
-			this.x = x;
-			this.y = y;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
+    }
 
-	static void input() throws IOException {
-		st = new StringTokenizer(br.readLine(), " ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		ans = N * M;
+    static void input() throws IOException {
+        st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        adj = new int[N][M];
+        visit = new int[N][M];
+        cctv = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                adj[i][j] = Integer.parseInt(st.nextToken());
+                if (adj[i][j] >= 1 && adj[i][j] <= 5) {
+                    cctv.add(new int[]{i, j});
+                }
+            }
+        }
+    }
 
-		A = new int[N][M];
-		cctvList = new ArrayList<>();
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < M; j++) {
-				A[i][j] = Integer.parseInt(st.nextToken());
-				if (A[i][j] > 0 && A[i][j] < 6) {
-					cctvList.add(new Cctv(A[i][j], i, j));
-				}
-			}
-		}
-	}
+    static void pro() {
+        // 1번 1개씩, 2번: 13 or 24, 3번: 14 or 21 or 32 or 43
+        // 4번: 134 or 241 or 312 or 423, 5번: 1234
+        dfs(0);
 
-	static void bfs(Cctv c, int d) { // watch
-		Queue<Cctv> q = new LinkedList<>();
+        System.out.print(ans);
+    }
 
-		q.add(c);
+    static void dfs(int depth) {
+        if (depth == cctv.size()) { // cctv의 모든 방향을 결정
+            ans = Math.min(ans, calculate());
+            return;
+        }
 
-		while (!q.isEmpty()) {
-			Cctv cctv = q.poll();
-			int nx = cctv.x + dir[d][0];
-			int ny = cctv.y + dir[d][1];
+        int[] cur = cctv.get(depth);
+        int x = cur[0];
+        int y = cur[1];
+        int t = adj[x][y];
 
-			if (nx < 0 || ny < 0 || nx >= N || ny >= M || B[nx][ny] == 6) {
-				break;
-			}
+        for (int[] d : dirs[t]) {
+            for (int k : d) {
+                update(x, y, k, 1);
+            }
 
-			if (B[nx][ny] == 0) { // 빈 칸이면
-				B[nx][ny] = -1;
-			}
-			q.add(new Cctv(c.type, nx, ny));
-		}
-	}
+            dfs(depth + 1);
 
-	static void watch(Cctv c, int d) {
-		int t = c.type;
+            // 복구
+            for (int k : d) {
+                update(x,y,k, -1);
+            }
+        }
+    }
 
-		if (t == 1) {
-			bfs(c, d); // 선택된 방향 감시
-		} else if (t == 2) {
-			if (d == 0 || d == 2) { // 상하
-				bfs(c, 0);
-				bfs(c, 2);
-			} else { // 좌우
-				bfs(c, 1);
-				bfs(c, 3);
-			}
-		} else if (t == 3) {
-			if (d == 0) { // 상우
-				bfs(c, 0);
-				bfs(c, 1);
-			} else if (d == 1) { // 우하
-				bfs(c, 1);
-				bfs(c, 2);
-			} else if (d == 2) { // 하좌
-				bfs(c, 2);
-				bfs(c, 3);
-			} else { // 상좌
-				bfs(c, 0);
-				bfs(c, 3);
-			}
-		} else if (t == 4) {
-			if (d == 0) {
-				bfs(c, 0); // 좌상우
-				bfs(c, 1);
-				bfs(c, 3);
-			} else if (d == 1) {
-				bfs(c, 0); // 상우하
-				bfs(c, 1);
-				bfs(c, 2);
-			} else if (d == 2) {
-				bfs(c, 1); // 좌하우
-				bfs(c, 2);
-				bfs(c, 3);
-			} else {
-				bfs(c, 0); // 상좌하
-				bfs(c, 2);
-				bfs(c, 3);
+    static void update(int x, int y, int k, int val) {
+        while (true) {
+            x += dir[k][0];
+            y += dir[k][1];
 
-			}
-		} else { // 상하좌우
-			bfs(c, 0);
-			bfs(c, 1);
-			bfs(c, 2);
-			bfs(c, 3);
-		}
-	}
+            if (x < 0 || y < 0 || x >= N || y >= M) {
+                break;
+            }
+            if (adj[x][y] == 6) {
+                break;
+            }
+            if (adj[x][y] == 0) {
+                visit[x][y] += val;
+            }
+        }
+    }
 
-	static int calculate() {
-		int cnt = 0;
-		for (int[] row : B) {
-			for (int value : row) {
-				if (value == 0) {
-					cnt++;
-				}
-			}
-		}
-
-		return cnt;
-	}
-
-	static void rec_func(int k, int cctvCnt) {
-		if (k == cctvCnt) { // 모든 cctv 감시 방향이 정해지면
-			B = new int[N][M];
-			for (int i = 0; i < N; i++) {
-				B[i] = A[i].clone();
-			}
-
-			for (int i = 0; i < cctvList.size(); i++) {
-				watch(cctvList.get(i), selected[i]);
-			}
-			ans = Math.min(ans, calculate());
-		} else {
-			for (int i = 0; i < 4; i++) {
-				selected[k] = i;
-				rec_func(k + 1, cctvCnt);
-				selected[k] = 0;
-			}
-		}
-	}
-
-	static void pro() {
-		// CCTV
-		int cnt = cctvList.size();
-		selected = new int[cnt];
-
-		rec_func(0, cnt);
-
-		System.out.println(ans);
-	}
-
-	public static void main(String[] args) throws IOException {
-		input();
-		pro();
-	}
-
+    static int calculate() {
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (adj[i][j] == 0 && visit[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
