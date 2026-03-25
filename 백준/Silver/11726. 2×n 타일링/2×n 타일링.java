4188f048d9da4bb1b101bf8b0fa41e52c24b70bd
posty <ykmxxi97@gmail.com>
2026-03-25 14:37:50 +0900

    [Silver III] Title: 2×n 타일링, Time: 108 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,47 +1,40 @@
-/*
-    2 x N 크기의 직사각형을 1 x 2, 2 x 1 타일로 채우는 방법의 수를 구하는 문제
-    입력 첫째 줄: N (1 <= N <= 1,000)
-    출력 첫째 줄: 2 x N 크기의 직사각형을 채우는 방법의 수를 10,007로 나눈 나머지
-    
-    가짜문제: Dy[i] = 2 x i 크기의 직사각형을 채우는 방법의 수
-    진짜문제: Dy[N] = 2 x N 크기의 직사각형을 채우는 방법의 수
-    초기값: Dy[1] = 1. Dy[2] = 2
-    점화식: Dy[i] = Dy[i - 2] + Dy[i - 1]
-        끝나는 타일이 1 x 2 타일 2개와, 2 x 1 타일일 때
-    
-*/
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-class Main {
+// 시간 복잡도: O(1)
+// 공간 복잡도: O(1)
+public class Main {
+
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    static final int MOD = 10007;
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
     static int N;
-    static int[] Dy;
-    
+    static int[] dp;
+    static int MOD = 10_007;
+
+    public static void main(String[] args) throws IOException {
+            input();
+            pro();
+    }
+
     static void input() throws IOException {
         N = Integer.parseInt(br.readLine());
     }
-    
+
     static void pro() {
-        Dy = new int[1005];
-        
-        Dy[1] = 1;
-        Dy[2] = 2;
-        
-        for (int i = 3; i <= 1000; i++) {
-            Dy[i] = (Dy[i - 1] + Dy[i - 2]) % MOD;
-        }
-        
-        System.out.println(Dy[N]);
-        
+        // 마지막에 1x2 타일 2개로 채울 것인가? 아니면 2x1로 채울것인가
+        pre();
+
+        System.out.print(dp[N]);
     }
-    
-    public static void main(String[] args) throws IOException {
-        input();
-        pro();
+
+    static void pre() {
+        dp = new int[1005]; // 2x1000 까지 미리 채워둔다
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= 1000; i++) {
+            dp[i] = ((dp[i - 1] % MOD) + (dp[i - 2] % MOD)) % MOD; // 마자막을 1x2로 채운다 + 마지막을 2x2로 채운다
+        }
     }
 }
-
